f631bccab8f961c1d71f530aa762cf5118bd8592
nova_44056 <senguy44056@gmail.com>
2023-05-20 11:20:22 -0500

    add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,28 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/globals.css";
 import { Toast } from "../components/toast";
+import { Router } from "next/router";
+import NProgress from "nprogress";
+import "../styles/nprogress.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   // TODO: use logger to avoid console logging out error and control the log in production
+  React.useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
+
   return (
     <>
       <Head>
